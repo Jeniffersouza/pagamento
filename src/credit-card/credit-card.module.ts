bd5fe02a26f447dc00c03e7efd5b8b10073b4298
Thiago Mendes <thiago.mendes@evoluum.com.br>
2023-12-21 18:22:12 -0300

    chore: add connection to rabbitmq

--- src/credit-card/credit-card.module.ts
+++ src/credit-card/credit-card.module.ts
@@ -2,8 +2,25 @@ import { Module } from '@nestjs/common';
 import { CreditCardController } from './credit-card.controller';
 import { CreditCardService } from './credit-card.service';
 import { PrismaService } from 'src/prisma.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NOTIFICATION_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:123456@localhost'],
+          queue: 'notification',
+          noAck: false,
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [CreditCardController],
   providers: [CreditCardService, PrismaService],
 })
